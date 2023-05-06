a536c911bd341c6566629b53fca43a1bfefd8eaa
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-05-06 17:05:55 +0100

    Fix linters errors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
-   def index
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invallide email or password"
-   end
+  def index
+    flash.now[:notice] = 'Logged in successfully'
+    flash.now[:alert] = 'Invallide email or password'
+  end
 end
