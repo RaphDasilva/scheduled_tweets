ce1c8292b8d478a4d14b64a2c2a4d7aa9f265719
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-05-10 09:07:07 +0100

    Fix rubocop errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,20 @@
 class SessionsController < ApplicationController
+  def new; end
 
-    def new
-        
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash[:alert] = 'Invalide email or password'
+      render :new
     end
+  end
 
-    def create
-     @user = User.find_by(email: params[:email])
-     if @user and @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to root_path, notice: "Logged in successfully"
-     else
-        flash[:alert] = "Invalide email or password"
-        render :new
-     end
-    end
+  def destroy
+    session[:user_id] = nil
 
-    def destroy
-        session[:user_id] = nil
-
-        redirect_to root_path, notice: "Logged out"
-    end   
+    redirect_to root_path, notice: 'Logged out'
+  end
 end
